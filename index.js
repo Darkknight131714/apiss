1484bb6077e408b2b340f6fb2ccaf8233c20b0b8
Gautam <darkknight131714@gmail.com>
2023-03-28 14:48:54 +0530

    Second

--- index.js
+++ index.js
@@ -2,16 +2,19 @@ const express=require('express');
 const db=require('./config');
 const firebase=require('firebase/app');
 const {getFirestore,collection,getDocs} =require('firebase/firestore/lite');
+const {query, orderBy,where} = require('firebase/firestore');
 const app=express();
 app.use(express.json());
 
 
 app.get('/',async (req,res)=>{
-    const usersCollection=collection(db,'imageurl');
-    const usersSnapshot=await getDocs(usersCollection);
-    const usersList=usersSnapshot.docs.map(doc=>doc.data());
-    res.send(usersList);
+        const usersCollection=collection(db,"imageurl");
+        const q=query(collection(db,"imageurl"));
+        const usersSnapshot=await getDocs(q);
+        const usersList=usersSnapshot.docs.map(doc=>doc.data());
+        res.send(usersList);
+    
 });
 
-app.listen(4000);
+app.listen(3000);
 console.log("Server started");
